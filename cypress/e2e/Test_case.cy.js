fc9a9d092254438dcea5f8041a74f2dc9d6796c8
thxnx36 <thxnx36@gmail.com>
2023-05-11 16:03:10 +0700

    read

--- cypress/e2e/Test_case.cy.js
+++ cypress/e2e/Test_case.cy.js
@@ -25,6 +25,8 @@ it('news&activities page', () => {
 
 })
 
+
+
 it('Create data', () => {
 
   cy.visit('https://coe-demo.ntpscrt.com/admin/login')
@@ -34,10 +36,29 @@ it('Create data', () => {
   cy.get(':nth-child(9) > .text-sm > :nth-child(2) > .items-center > .flex > span').click()
   cy.wait(1000)
   cy.get('.filament-button').click()
-  cy.get('[id="data.title"]').type('Test1')
+  cy.get('[id="data.title"]').type('Test')
   cy.get('[id="data.content"]').type('Test create data content')
   cy.get('.filament-page-actions > .text-white').click()
+  cy.get(':nth-child(9) > .text-sm > :nth-child(2) > .items-center > .flex > span').click()
+  cy.wait(1000)
+
+  ///check in home page
+  cy.visit('https://coe-demo.ntpscrt.com/home')
+  cy.get('.mb-6 > .max-w-sm > :nth-child(1)').scrollIntoView({ duration: 2000 })
+  cy.wait(1000)
 
 })
 
+it('Read data', () => {
 
+  cy.visit('https://coe-demo.ntpscrt.com/admin/login')
+
+  cy.get('[type="email"]').type('admin@admin.com')
+  cy.get('[type="password"]').type('secret{Enter}')
+  cy.get(':nth-child(9) > .text-sm > :nth-child(2) > .items-center > .flex > span').click()
+  cy.wait(1000)
+  cy.get('#tableSearchInput').type("test")
+
+
+
+})
